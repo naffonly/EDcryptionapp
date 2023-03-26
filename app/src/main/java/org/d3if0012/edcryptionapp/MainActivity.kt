67e4205540aa53b0065805c78708066c581f145a
naffonly <nafjie02@gmail.com>
2023-03-26 21:13:34 +0700

    add feature copy to clipboard

--- app/src/main/java/org/d3if0012/edcryptionapp/MainActivity.kt
+++ app/src/main/java/org/d3if0012/edcryptionapp/MainActivity.kt
@@ -1,11 +1,14 @@
 package org.d3if0012.edcryptionapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import org.d3if0012.edcryptionapp.databinding.ActivityMainBinding
-import java.util.Base64
+import org.w3c.dom.Text
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -20,8 +23,16 @@ class MainActivity : AppCompatActivity() {
             encode()
         }
 
+        binding.encodeCopyBotton.setOnClickListener {
+            encodeCopy()
+        }
+
         binding.decodeBotton.setOnClickListener {
             decode()
+        }
+
+        binding.decodeCopyBotton.setOnClickListener {
+            decodeCopy()
         }
 
 
@@ -40,7 +51,7 @@ class MainActivity : AppCompatActivity() {
         binding.decodeInput.setText(encoded)
         }catch (e: Exception){
             e.printStackTrace()
-            Toast.makeText(applicationContext, "Text tidak valid untuk di Encode", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Text tidak valid untuk di Encode", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -57,7 +68,40 @@ class MainActivity : AppCompatActivity() {
         binding.encodeInput.setText(decoded)
     }catch (e: Exception){
         e.printStackTrace()
-        Toast.makeText(applicationContext, "Text tidak valid untuk di Decode", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "Text tidak valid untuk di Decode", Toast.LENGTH_SHORT).show()
         }
     }
+
+    private  fun encodeCopy(){
+
+        val encodeText = binding.encodeInput.text.toString()
+
+        if (TextUtils.isEmpty(encodeText)){
+            Toast.makeText(this, "Isian Masih Kosong,Tidak dapat disalin", Toast.LENGTH_SHORT).show();
+        }else{
+            val clipboard = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = ClipData.newPlainText("text label", encodeText)
+            clipboard.setPrimaryClip(clip)
+            Toast.makeText(this, "Text Disalin", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private  fun decodeCopy(){
+
+
+        val decodeText = binding.decodeInput.text.toString()
+
+        if (TextUtils.isEmpty(decodeText)){
+            Toast.makeText(this, "Isian Masih Kosong,Tidak dapat disalin", Toast.LENGTH_SHORT).show();
+        }else{
+
+            val clipboard = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = ClipData.newPlainText("text label", decodeText)
+            clipboard.setPrimaryClip(clip)
+            Toast.makeText(this, "Text Disalin", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
 }
