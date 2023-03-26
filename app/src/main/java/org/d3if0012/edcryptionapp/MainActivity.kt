ea1174cff334fe5ed2fec58299321971981380c8
naffonly <nafjie02@gmail.com>
2023-03-26 20:56:41 +0700

    add logic decode and encode text

--- app/src/main/java/org/d3if0012/edcryptionapp/MainActivity.kt
+++ app/src/main/java/org/d3if0012/edcryptionapp/MainActivity.kt
@@ -2,7 +2,10 @@ package org.d3if0012.edcryptionapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 import org.d3if0012.edcryptionapp.databinding.ActivityMainBinding
+import java.util.Base64
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -11,5 +14,50 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        binding.EncodeBotton.setOnClickListener {
+            encode()
+        }
+
+        binding.decodeBotton.setOnClickListener {
+            decode()
+        }
+
+
+    }
+
+    private fun encode(){
+        val encodeText = binding.encodeInput.text.toString()
+        if (TextUtils.isEmpty(encodeText)){
+            Toast.makeText(this, "Text tidak Kosong", Toast.LENGTH_SHORT).show()
+        }
+
+        try {
+        val text = encodeText
+        val encoder: Base64.Encoder = Base64.getEncoder()
+        val encoded: String = encoder.encodeToString(text.toByteArray())
+        binding.decodeInput.setText(encoded)
+        }catch (e: Exception){
+            e.printStackTrace()
+            Toast.makeText(applicationContext, "Text tidak valid untuk di Encode", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun decode(){
+        val decodeText = binding.decodeInput.text.toString()
+        if (TextUtils.isEmpty(decodeText)){
+            Toast.makeText(this, "Text tidak Kosong", Toast.LENGTH_SHORT).show()
+        }
+
+    try {
+        val dtext = decodeText
+        val decoder: Base64.Decoder = Base64.getMimeDecoder()
+        val decoded  = String(decoder.decode(dtext))
+        binding.encodeInput.setText(decoded)
+    }catch (e: Exception){
+        e.printStackTrace()
+        Toast.makeText(applicationContext, "Text tidak valid untuk di Decode", Toast.LENGTH_SHORT).show()
+        }
     }
 }
